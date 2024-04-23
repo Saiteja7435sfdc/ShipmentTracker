26b0ce4076b83081b1ca6964b9c403b600466ef4
Saiteja7435sfdc <167534007+Saiteja7435sfdc@users.noreply.github.com>
2024-04-23 11:20:21 -0400

    Update trackShipmentStatus.js

--- trackShipmentStatus.js
+++ trackShipmentStatus.js
@@ -1,6 +1,7 @@
 import { LightningElement, api, wire } from "lwc";
 import { getRecord } from "lightning/uiRecordApi";
 import SHIPMENT_TRACKINGNUMBER_FIELD from '@salesforce/schema/Shipment.TrackingNumber';
+import getCurrentTrackingStatus from "@salesforce/apex/TrackShipmentStatusController.getCurrentStatus";
 import getTrackingStatus from "@salesforce/apex/TrackShipmentStatusController.getTrackingStatus";
 
 const FIELDS = [SHIPMENT_TRACKINGNUMBER_FIELD];
@@ -8,30 +9,13 @@ const FIELDS = [SHIPMENT_TRACKINGNUMBER_FIELD];
 export default class TrackShipmentStatus extends LightningElement {
     trackingStatus;
     @api recordId;
-
-    @wire(getRecord, { recordId: "$recordId", fields: FIELDS })
-    shipment;
-
-    connectedCallback() {
-        const mapInput = {
-            trackingNumber: this.trackingNumber
-        };
-        getTrackingStatus({
-            mapInput
-        })
-        .then(result => {
-            if(result.isCalloutFailed) {
-                // TO DO: Show the toast message
-                console.error(`Callout Failed!!!`)
-            }
-            else {
-                this.trackingStatus = result.status;
-            }
-        })
-        .catch(error => console.error(error))
-    }
-
-    get trackingNumber() {
-        return this.shipment?.data?.fields?.Name.value;
+  @wire(getCurrentTrackingStatus, { recordid:  "$recordId" })
+    wiredData({ error, data }) {
+      if (data) {
+        console.log('Data', data);
+        this.trackingStatus = data;
+      } else if (error) {
+         console.error('Error:', error);
+      }
     }
 }
